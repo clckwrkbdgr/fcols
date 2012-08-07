e7ac4eef9d1d4c3ce9b82d0ac9ac581f3e14058f
Igor Chaika <kp580bm1@gmail.com>
2012-08-07 13:19:11 +0000

    Added corrent formatting. Still has incorrect positions determination.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat test/addcolumn | ./fcols '("' | diff - test/addcolumn.result || exit 1
+#cat test/addcolumn | ./fcols '("' | diff - test/addcolumn.result || exit 1
 cat test/countzero | ./fcols ' ' ' ' ' ' ' ' | diff - test/countzero.result || exit 1
 cat test/newrow | ./fcols '<<' '<<' '<<' '<<' | diff - test/newrow.result || exit 1
 cat test/qfetch | ./fcols ' ' | diff - test/qfetch.result || exit 1
